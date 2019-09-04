65f51adebb082ba56c25e50a0cd402e8dccbf6dc
hayden <haydenwb@gmail.com>
2019-09-04 18:23:30 +0100

    mvp and extensions done but I don't understand extensions

--- models/traveller.js
+++ models/traveller.js
@@ -3,34 +3,48 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-  journeyStartLocations = this.journeys.map(function (journey) {
+  const journeyStartLocations = this.journeys.map(function (journey) {
     return journey.startLocation;
   })
   return journeyStartLocations;
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-  journeyEndLocations = this.journeys.map(function (journey) {
+  const journeyEndLocations = this.journeys.map(function (journey) {
     return journey.endLocation;
   })
   return journeyEndLocations;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
-};
+  const foundJourneys = this.journeys.filter((journey) => {
+    if (journey.transport === transport) return journey;
+  });
+  return foundJourneys;
+}
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+ const foundJourneys = this.journeys.filter((journey) => {
+  if (journey.distance > minDistance) return journey;
+ });
+ return foundJourneys;
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0);
 };
-
-Traveller.prototype.getUniqueModesOfTransport = function () {
-
-};
-
 
 module.exports = Traveller;
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  allVehicles = this.journeys.map(function (journey){
+    return journey.transport;
+  })
+
+  uniqueVehicles = allVehicles.filter(function (singleVehicle, index) {
+    return allVehicles.indexOf(singleVehicle) === index;
+  });
+  return uniqueVehicles;
+};
